3ea5d236c8f33643be53feba0cfcc0f95d3024e3
Jorge Esteban Gómez <jegomez1907@gmail.com>
2024-02-29 19:46:40 -0500

    create packages

---
+++ 00_packages.R
@@ -0,0 +1,8 @@
+
+
+
+require(pacman)
+
+p_load(tidyverse, rio, readr, tidymodels, 
+       stargazer, knitr, kableExtra, 
+       readxl, purrr, fastDummies)
--- scripts/01_add_new_var.R
+++ scripts/01_add_new_var.R
@@ -8,11 +8,7 @@ rm(list = ls())
 
 # Cargo paquetes
 
-require(pacman)
 
-p_load(tidyverse, rio, readr, tidymodels, 
-       stargazer, knitr, kableExtra, 
-       readxl, purrr, fastDummies)
 
 # Cargar bases mensuales por modulos y limpiar
 
